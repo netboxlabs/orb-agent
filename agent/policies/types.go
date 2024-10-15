f3aec018e80d6510b1d4de7a4f657e606d1b4f02
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2024-10-15 16:33:55 -0300

    remove license comments

--- agent/policies/types.go
+++ agent/policies/types.go
@@ -1,13 +1,10 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 package policies
 
 import (
 	"database/sql/driver"
-	_ "github.com/mattn/go-sqlite3"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type PolicyData struct {
--- agent/rpc_from.go
+++ agent/rpc_from.go
@@ -1,14 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 package agent
 
 import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/eclipse/paho.mqtt.golang"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/orb-community/orb/fleet"
 	"go.uber.org/zap"
 )
