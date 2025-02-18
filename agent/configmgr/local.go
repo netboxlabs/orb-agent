703a4ca3e4cdb6df00dbbe66d8ba6ca9b295f1ec
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2025-02-18 17:32:29 -0300

    fix lint

--- agent/configmgr/local.go
+++ agent/configmgr/local.go
@@ -33,8 +33,10 @@ func (lc *localConfigManager) Start(cfg config.Config, backends map[string]backe
 		for pName, data := range policy {
 			policyID := uuid.NewSHA1(uuid.Nil, []byte(pName+beName)).String()
 			id := uuid.NewString()
-			payload := config.PolicyPayload{ID: policyID, Action: "manage",
-				Name: pName, DatasetID: id, Backend: beName, Version: 1, Data: data}
+			payload := config.PolicyPayload{
+				ID: policyID, Action: "manage",
+				Name: pName, DatasetID: id, Backend: beName, Version: 1, Data: data,
+			}
 			lc.pMgr.ManagePolicy(payload)
 		}
 
