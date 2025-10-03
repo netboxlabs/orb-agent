41a39c81b055cfed583d1bd1658a1b16e93af20d
James Jeffries <jjeffries@netboxlabs.com>
2025-10-03 17:16:46 +0100

    started extracting handlers and requests from connection

--- agent/configmgr/fleet/connection.go
+++ agent/configmgr/fleet/connection.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/netboxlabs/orb-agent/agent/backend"
 	"github.com/netboxlabs/orb-agent/agent/configmgr/fleet/messages"
-	"github.com/netboxlabs/orb-agent/agent/version"
 )
 
 // MQTTConnection manages the MQTT connection
@@ -182,99 +181,4 @@ func (connection *MQTTConnection) Connect(ctx context.Context, fleetMQTTURL, tok
 func (connection *MQTTConnection) Disconnect(ctx context.Context) error {
 	connection.heartbeater.stop()
 	return connection.connectionManager.Disconnect(ctx)
-}
-
-func (connection *MQTTConnection) sendGroupMembershipsRequest(ctx context.Context, publishFunc func(ctx context.Context, payload []byte) error) {
-	body, err := json.Marshal(messages.RPC{
-		// SchemaVersion: messages.CurrentRPCSchemaVersion, // TODO: add schema version check later
-		Func:    "group_membership_req",
-		Payload: messages.SendGroupMembershipsRequest{},
-	})
-	if err != nil {
-		connection.logger.Error("backend failed to marshal capabilities, skipping", "error", err)
-		return
-	}
-
-	connection.logger.Info("sending group memberships request", "value", string(body))
-	err = publishFunc(ctx, body)
-	if err != nil {
-		connection.logger.Error("error sending group memberships request", "error", err)
-	}
-	connection.logger.Info("group memberships request sent", "value", string(body))
-}
-
-func (connection *MQTTConnection) sendCapabilities(ctx context.Context, backends map[string]backend.Backend, labels map[string]string, publishFunc func(ctx context.Context, payload []byte) error) {
-	capabilities := messages.Capabilities{
-		SchemaVersion: messages.CurrentCapabilitiesSchemaVersion,
-		AgentLabels:   labels,
-		OrbAgent: messages.OrbAgentInfo{
-			Version: version.GetBuildVersion(),
-		},
-	}
-
-	capabilities.Backends = make(map[string]messages.BackendInfo)
-	for name, be := range backends {
-		ver, err := be.Version()
-		if err != nil {
-			connection.logger.Error("backend failed to retrieve version, skipping", "backend", name, "error", err)
-			continue
-		}
-		cp, err := be.GetCapabilities()
-		if err != nil {
-			connection.logger.Error("backend failed to retrieve capabilities, skipping", "backend", name, "error", err)
-			continue
-		}
-		capabilities.Backends[name] = messages.BackendInfo{
-			Version: ver,
-			Data:    cp,
-		}
-	}
-
-	body, err := json.Marshal(capabilities)
-	if err != nil {
-		connection.logger.Error("backend failed to marshal capabilities, skipping", "error", err)
-		return
-	}
-
-	connection.logger.Info("sending capabilities", "value", string(body))
-	err = publishFunc(ctx, body)
-	if err != nil {
-		connection.logger.Error("error sending capabilities", "error", err)
-	}
-}
-
-func (connection *MQTTConnection) dispatchToHandlers(messageType string, rpc messages.RPC, orgID string) {
-	switch messageType {
-	case "group_membership":
-		connection.handleGroupMemberships(rpc, orgID)
-	default:
-		connection.logger.Debug("unknown message type", "message_type", messageType)
-	}
-}
-
-func (connection *MQTTConnection) handleGroupMemberships(rpc messages.RPC, orgID string) {
-	connection.logger.Debug("handling group memberships", "payload", rpc.Payload)
-	payloadJSON, err := json.Marshal(rpc.Payload)
-	if err != nil {
-		connection.logger.Error("failed to marshal payload", "error", err)
-		return
-	}
-	groupMeberships := messages.GroupMemberships{}
-	if err := json.Unmarshal(payloadJSON, &groupMeberships); err != nil {
-		connection.logger.Error("failed to unmarshal payload", "error", err)
-		return
-	}
-
-	for _, group := range groupMeberships.Groups {
-		connection.logger.Info("subscribing to group", "group", group)
-		_, err := connection.connectionManager.Subscribe(context.Background(), &paho.Subscribe{
-			Subscriptions: []paho.SubscribeOptions{
-				{Topic: groupTopic(orgID, group.GroupID), QoS: 1},
-			},
-		})
-		if err != nil {
-			connection.logger.Error("failed to subscribe to group", "error", err)
-		}
-		connection.logger.Info("subscribed to group topic for group ID", "group_id", group.GroupID)
-	}
 }
