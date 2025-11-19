a195f9c2f2a6c72f8ed6deda559f89a6a9a72653
James Jeffries <jjeffries@netboxlabs.com>
2025-11-19 15:18:24 +0000

    be consistent with log level casing

--- agent/backend/snmpdiscovery/snmp_discovery.go
+++ agent/backend/snmpdiscovery/snmp_discovery.go
@@ -114,9 +114,9 @@ func (d *snmpDiscoveryBackend) Configure(logger *slog.Logger, repo policies.Poli
 	if logLevel, prs := config["log_level"].(string); prs {
 		d.diodeLogLevel = logLevel
 	} else if debug, prs := config["debug"].(bool); prs && debug {
-		d.diodeLogLevel = "DEBUG"
+		d.diodeLogLevel = "debug"
 	} else if logger.Enabled(context.Background(), slog.LevelDebug) {
-		d.diodeLogLevel = "DEBUG"
+		d.diodeLogLevel = "debug"
 	}
 
 	if common.Otlp.Grpc != "" {
